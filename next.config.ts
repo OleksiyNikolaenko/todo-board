ed7a55a088684c90289489d16847af1bfd118fae
Oleksiy <nikolaenkooleksiy@gmail.com>
2025-08-14 10:27:56 +0300

    deleted svgr

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  webpack(config) {
-    const fileLoaderRule = config.module.rules.find((rule) =>
-      rule?.test?.test?.('.svg'),
-    );
-
-    fileLoaderRule.exclude = /\.svg$/;
-
-    config.module.rules.push({
-      test: /\.svg$/,
-      issuer: /\.[jt]sx?$/,
-      use: ['@svgr/webpack'],
-    });
-
-    return config;
-  },
+  /* config options here */
 };
 
 export default nextConfig;
